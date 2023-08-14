b2ad9d782347162d301bcfaf20fc43adcb9e3740
rutaitp <rk2906@nyu.edu>
2023-08-14 16:50:18 +0300

    code

---
+++ app.js
@@ -0,0 +1,40 @@
+let story = ["I", "found", "a", "picture", "on", "my", "phone", "sleeping.", "I", "live", "alone."];
+
+window.addEventListener('load', () => {
+    let counter = 0;
+    let container = document.getElementById('container');
+
+    //listen for clicks on the page and get an exact location
+    window.addEventListener('click', (e) => {
+        console.log('you have clicked here, ' + e.x + ',' + e.y);
+        console.log('counter: ', counter);
+
+        //remove all html elements if story ends
+        if (counter > 10) {
+            console.log('cleaning up all content');
+            //remove all children
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+              }
+            counter = 0; // reset to the beginning
+        } else { //otherwise keep adding html elements
+            let span = document.createElement('span');
+            span.innerHTML = story[counter];
+            span.style.position = "absolute";
+            span.style.top = `${e.y}px`;
+            span.style.left = `${e.x}px`;
+            //add random color
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            span.style.color = `rgb(${r}, ${g}, ${b})`;
+            //add random size
+            span.style.fontSize = Math.floor(Math.random() * 72)+"px";
+            container.appendChild(span);
+            counter++;
+        }
+    });
+});
+
+
+
